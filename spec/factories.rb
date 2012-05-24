9572d0e05813ad7e0b29e0ebd8e36026898a3c2d
Jonathan Paez <jon.paez@gmail.com>
2012-05-24 08:10:01 +0000

    Factory-generated users: Remove personal info from factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 #By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
-  user.name                   "Jonathan Paez"
-  user.email                  "jpaez@example.com"
+  user.name                   "Factory User"
+  user.email                  "factory@example.com"
   user.password               "foobar"
   user.password_confirmation  "foobar"
 end
