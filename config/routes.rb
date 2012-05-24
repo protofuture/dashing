d34cd838f979ff20f35004116866f9d80de87feb
Jonathan Paez <jon.paez@gmail.com>
2012-05-24 09:12:45 +0000

    Removed /contact and /help pages and associated tests

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,7 @@ Box::Application.routes.draw do
   match '/signin',  :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
 
-  match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
-  match '/help',    :to => 'pages#help'
 
   root :to => 'pages#home'
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -20,19 +20,6 @@ describe PagesController do
     end
   end
 
-  describe "GET 'contact'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get 'contact'
-      response.should have_selector("title",
-			:content => @base_title + " | Contact")
-    end
-  end
-
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
